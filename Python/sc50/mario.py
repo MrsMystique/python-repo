0f6dcc80236044a0d1e79a4b2d5acaa6c8c90dad
Mystique <zorinatrader@gmail.com>
2023-03-22 16:17:10 +0300

    -

--- Python/sc50/mario.py
+++ Python/sc50/mario.py
@@ -1,12 +1,15 @@
 def main():
     heigth = get_heigth()
+
     for i in range(heigth):
         print("#"*3)
-        
-def get_heigth():
+
+
+def get_heigth(n):
     while True:
         n = int(input("Heigth: "))
         if n > 0:
             return n
-        
+
+
 main()
