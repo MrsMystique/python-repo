6f0babfbe68ed372fc6576617f01f7fa0ddcbf36
Mystique <zorinatrader@gmail.com>
2023-04-04 18:56:09 +0300

    uppercase

--- PythonPhonebook/phonebook_functions.py
+++ PythonPhonebook/phonebook_functions.py
@@ -43,15 +43,15 @@ def choose_option():
 # Функция для добавления контакта
 def add_contact():
     with open("phonebook.csv", "a") as file:
-        columnnames = ["Name", "Lastname", "Phone", "Email", "Telegram", "Organization name"]
-        name = input("Введите имя: ").strip().title()
-        lastname = input("Введите Фамилию: ").strip().title()
-        phone_number = input("Введите номер телефона: ").strip()
-        email = input("Введите email: ")
-        telegram_id = input("Введите telegram_id: ").strip()
-        Organization = input("Введите имя организации: ").strip().title()
-        writer = csv.DictWriter(file, columnnames)
-        writer.writerow({"Name": name, "Lastname": lastname, "Phone": phone_number, "Email": email, "Telegram": telegram_id,"Organization name": Organization})
+        columnNAMEs = ["NAME", "LASTNAME", "PHONE", "EMAIL", "TELEGRAM", "ORGANIZATION NAME"]
+        NAME = input("Введите имя: ").strip().upper()
+        LASTNAME = input("Введите Фамилию: ").strip().upper()
+        PHONE_number = input("Введите номер телефона: ").strip()
+        EMAIL = input("Введите EMAIL: ")
+        TELEGRAM_id = input("Введите TELEGRAM_id: ").strip()
+        ORGANIZATION = input("Введите имя организации: ").strip().upper()
+        writer = csv.DictWriter(file, columnNAMEs)
+        writer.writerow({"NAME": NAME, "LASTNAME": LASTNAME, "PHONE": PHONE_number, "EMAIL": EMAIL, "TELEGRAM": TELEGRAM_id,"ORGANIZATION NAME": ORGANIZATION})
     print('Контакт добавлен!')
 
 
@@ -66,12 +66,12 @@ def search_contacts():
     # выводим найденные данные в форме визитной карточки
     for index, row in filtered_df.iterrows():
         print('===============================')
-        print('Name:', row['Name'])
-        print('Lastname:', row['Lastname'])
-        print('Phone:', row['Phone'])
-        print("Email:", row["Email"])
-        print("Telegram:", row["Telegram"])
-        print("Organization name:", row["Organization name"])
+        print('NAME:', row['NAME'])
+        print('LASTNAME:', row['LASTNAME'])
+        print('PHONE:', row['PHONE'])
+        print("EMAIL:", row["EMAIL"])
+        print("TELEGRAM:", row["TELEGRAM"])
+        print("ORGANIZATION NAME:", row["ORGANIZATION NAME"])
         print('===============================')
 
 
@@ -93,15 +93,15 @@ def export_contact_found_to_pdf():
 
     # выводим найденные данные в форме визитной карточки и добавляем QR-код с телеграм ай ди
     for index, row in filtered_df.iterrows():
-        pdf_file.drawString(100, y, 'Name: ' + str(row['Name']))
-        pdf_file.drawString(100, y-20, 'Lastname: ' + str(row['Lastname']))
-        pdf_file.drawString(100, y-40, 'Phone: ' + str(row['Phone']))
-        pdf_file.drawString(100, y-60, 'Email: ' + str(row['Email']))
-        pdf_file.drawString(100, y-80, 'Telegram: ' + str(row['Telegram']))
-        pdf_file.drawString(100, y-100, 'Organization name: ' + str(row['Organization name']))
+        pdf_file.drawString(100, y, 'NAME: ' + str(row['NAME']))
+        pdf_file.drawString(100, y-20, 'LASTNAME: ' + str(row['LASTNAME']))
+        pdf_file.drawString(100, y-40, 'PHONE: ' + str(row['PHONE']))
+        pdf_file.drawString(100, y-60, 'EMAIL: ' + str(row['EMAIL']))
+        pdf_file.drawString(100, y-80, 'TELEGRAM: ' + str(row['TELEGRAM']))
+        pdf_file.drawString(100, y-100, 'ORGANIZATION NAME: ' + str(row['ORGANIZATION NAME']))
 # генерируем QR-код с телеграммом
         qr = qrcode.QRCode(version=1, box_size=10, border=5)
-        qr.add_data('telegram.me/' + row['Telegram'])
+        qr.add_data('TELEGRAM.me/' + row['TELEGRAM'])
         qr.make(fit=True)
         img = qr.make_image(fill_color='black', back_color='white')
         img.save('qrcode.png')
@@ -128,12 +128,12 @@ def export_all_contacts_to_pdf():
 
     # выводим данные в форме визитной карточки и добавляем QR-код с телеграмм
     for index, row in df.iterrows():
-        pdf_file.drawString(100, y, 'Name: ' + str(row['Name']))
-        pdf_file.drawString(100, y-20, 'Lastname: ' + str(row['Lastname']))
-        pdf_file.drawString(100, y-40, 'Phone: ' + str(row['Phone']))
-        pdf_file.drawString(100, y-60, 'Email: ' + str(row['Email']))
-        pdf_file.drawString(100, y-80, 'Telegram: ' + str(row['Telegram']))
-        pdf_file.drawString(100, y-100, 'Organization name: ' + str(row['Organization name']))
+        pdf_file.drawString(100, y, 'NAME: ' + str(row['NAME']))
+        pdf_file.drawString(100, y-20, 'LASTNAME: ' + str(row['LASTNAME']))
+        pdf_file.drawString(100, y-40, 'PHONE: ' + str(row['PHONE']))
+        pdf_file.drawString(100, y-60, 'EMAIL: ' + str(row['EMAIL']))
+        pdf_file.drawString(100, y-80, 'TELEGRAM: ' + str(row['TELEGRAM']))
+        pdf_file.drawString(100, y-100, 'ORGANIZATION NAME: ' + str(row['ORGANIZATION NAME']))
         y -= 150  # смещаем координату по y для следующего контакта
 
     pdf_file.save()
@@ -146,7 +146,7 @@ def delete_contact():
     with open("phonebook.csv", "r", newline ='') as file:
         filereader = csv.DictReader(file)
         for row in filereader:
-            search.append({"Name": row["Name"], "Lastname": row["Lastname"], "Phone": row["Phone"], "Email": row["Email"], "Telegram": row["Telegram"], "Organization name": row["Organization name"]})
+            search.append({"NAME": row["NAME"], "LASTNAME": row["LASTNAME"], "PHONE": row["PHONE"], "EMAIL": row["EMAIL"], "TELEGRAM": row["TELEGRAM"], "ORGANIZATION NAME": row["ORGANIZATION NAME"]})
         matching_contacts = []
         for person in search:
             if any(search_term.lower() in value.lower() for value in person.values()):
@@ -156,10 +156,10 @@ def delete_contact():
             return
         print("Совпадающие контакты:")
         for i, contact in enumerate(matching_contacts):
-            print(f"{i}: {contact['Name']} {contact['Lastname']}, {contact['Phone']}, {contact['Email']}, {contact['Telegram']}, {contact['Organization name']}")
+            print(f"{i}: {contact['NAME']} {contact['LASTNAME']}, {contact['PHONE']}, {contact['EMAIL']}, {contact['TELEGRAM']}, {contact['ORGANIZATION NAME']}")
         index = int(input("Введите порядковый номер контакта для удаления: "))
-        fieldnames = ["Name", "Lastname", "Phone", "Email", "Telegram", "Organization name"]
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        fieldNAMEs = ["NAME", "LASTNAME", "PHONE", "EMAIL", "TELEGRAM", "ORGANIZATION NAME"]
+        writer = csv.DictWriter(file, fieldNAMEs=fieldNAMEs)
         writer.writeheader()
         for person in search:
             if person not in matching_contacts or person != matching_contacts[index]:
@@ -174,7 +174,7 @@ def edit_contact():
     with open("phonebook.csv", "r", newline ='') as file:
         filereader = csv.DictReader(file)
         for row in filereader:
-            search_result.append({"Name": row["Name"], "Lastname": row["Lastname"], "Phone": row["Phone"], "Email": row["Email"], "Telegram": row["Telegram"], "Organization name": row["Organization name"]})
+            search_result.append({"NAME": row["NAME"], "LASTNAME": row["LASTNAME"], "PHONE": row["PHONE"], "EMAIL": row["EMAIL"], "TELEGRAM": row["TELEGRAM"], "ORGANIZATION NAME": row["ORGANIZATION NAME"]})
         matching_contacts = []
         for contact in search_result:
             if any(search_term.lower() in value.lower() for value in contact.values()):
@@ -184,40 +184,40 @@ def edit_contact():
             return
         print("Совпадающие контакты:")
         for i, contact in enumerate(matching_contacts):
-            print(f"{i}: {contact['Name']} {contact['Lastname']}, {contact['Phone']}, {contact['Email']}, {contact['Telegram']}, {contact['Organization name']}")
+            print(f"{i}: {contact['NAME']} {contact['LASTNAME']}, {contact['PHONE']}, {contact['EMAIL']}, {contact['TELEGRAM']}, {contact['ORGANIZATION NAME']}")
         index = int(input("Введите порядковый номер контакта для изменения: "))
-        print(f"{matching_contacts[index]['Name']} {matching_contacts[index]['Lastname']}")
-        print(f"Изменение {matching_contacts[index]['Name']} {matching_contacts[index]['Lastname']}...")
+        print(f"{matching_contacts[index]['NAME']} {matching_contacts[index]['LASTNAME']}")
+        print(f"Изменение {matching_contacts[index]['NAME']} {matching_contacts[index]['LASTNAME']}...")
         print("Выберите поле, которое вы хотите отредактировать:")
         print("1. Номер телефона")
-        print("2. Email")
-        print("3. Telegram ID")
+        print("2. EMAIL")
+        print("3. TELEGRAM ID")
         print("4. Название организации")
         field_choice = input("Введите номер поля: ")
         if field_choice == "1":
-            new_phone_number = input(f"Введите новый номер для {matching_contacts[index]['Name']} {matching_contacts[index]['Lastname']}: ").strip()
-            matching_contacts[index]["Phone"] = new_phone_number
+            new_PHONE_number = input(f"Введите новый номер для {matching_contacts[index]['NAME']} {matching_contacts[index]['LASTNAME']}: ").strip()
+            matching_contacts[index]["PHONE"] = new_PHONE_number
         elif field_choice == "2":
-            new_email = input(f"Введите новый email для {matching_contacts[index]['Name']} {matching_contacts[index]['Lastname']}: ").strip()
-            matching_contacts[index]["Email"] = new_email
+            new_EMAIL = input(f"Введите новый EMAIL для {matching_contacts[index]['NAME']} {matching_contacts[index]['LASTNAME']}: ").strip()
+            matching_contacts[index]["EMAIL"] = new_EMAIL
         elif field_choice == "3":
-            new_telegram_id = input(f"Введите новый telegram id для {matching_contacts[index]['Name']} {matching_contacts[index]['Lastname']}: ").strip()
-            matching_contacts[index]["Telegram"] = new_telegram_id
+            new_TELEGRAM_id = input(f"Введите новый TELEGRAM id для {matching_contacts[index]['NAME']} {matching_contacts[index]['LASTNAME']}: ").strip()
+            matching_contacts[index]["TELEGRAM"] = new_TELEGRAM_id
         elif field_choice == "4":
-            new_organization = input(f"Введите наименование организации {matching_contacts[index]['Name']} {matching_contacts[index]['Lastname']}: ").strip()
-            matching_contacts[index]["Organization name"] = new_organization
+            new_ORGANIZATION = input(f"Введите наименование организации {matching_contacts[index]['NAME']} {matching_contacts[index]['LASTNAME']}: ").strip()
+            matching_contacts[index]["ORGANIZATION NAME"] = new_ORGANIZATION
         else:
             print("Некорректный выбор.")
             return
         with open("phonebook.csv", "w", newline='') as file:
-            writer = csv.DictWriter(file, fieldnames=["Name", "Lastname", "Phone", "Email", "Telegram", "Organization name"])
+            writer = csv.DictWriter(file, fieldNAMEs=["NAME", "LASTNAME", "PHONE", "EMAIL", "TELEGRAM", "ORGANIZATION NAME"])
             writer.writeheader()
             for contact in search_result:
                 if contact in matching_contacts:
                     writer.writerow(contact)
                 else:
                     writer.writerow(contact)
-            print(f"Контакт {matching_contacts[index]['Name']} {matching_contacts[index]['Lastname']} обновлен.")
+            print(f"Контакт {matching_contacts[index]['NAME']} {matching_contacts[index]['LASTNAME']} обновлен.")
 
 
 choose_option()
